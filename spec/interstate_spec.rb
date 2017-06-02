3e27cd837ba09d26228526a99128dd28634b9baf
Samuel Martini <samueljmartini@gmail.com>
2017-06-02 12:09:52 +0200

    Slightly change rspec message

--- spec/interstate_spec.rb
+++ spec/interstate_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe Interstate do
 
   describe '#states' do
     it 'returns all states' do
-      interstate.transition_table :off, :on {}
+      interstate.transition_table(:off, :on) {}
       instance = interstate.new
       expect(instance.states).to eq [:off, :on]
     end
@@ -41,23 +41,22 @@ RSpec.describe Interstate do
       allow_any_instance_of(Interstate::StateMachine).to receive(:evaluate_transition_by!)
       interstate.allow(event: :run, transition_to: [:running] , from: [:standing])
     end
-    context 'when is instantiated' do
-      let(:subject) { interstate.new }
 
-      it 'define a method from merging event and from arguments' do
-        expect(subject).to respond_to(:run_standing)
-      end
+    let(:subject) { interstate.new }
 
-      it 'creates a context rules' do
-        allow(Interactor::Context).to receive(:new).with(event: :run, transition_to: [:running] , from: [:standing])
-        subject.run_standing
-        expect(Interactor::Context).to have_received(:new).with(event: :run, transition_to: [:running] , from: [:standing])
-      end
+    it 'define a method from merging event and from arguments' do
+      expect(subject).to respond_to(:run_standing)
+    end
 
-      it 'evaluates the transition throught state machine' do
-        expect_any_instance_of(Interstate::StateMachine).to receive(:evaluate_transition_by!)
-        subject.run_standing
-      end
+    it 'creates a context rules' do
+      allow(Interactor::Context).to receive(:new).with(event: :run, transition_to: [:running] , from: [:standing])
+      subject.run_standing
+      expect(Interactor::Context).to have_received(:new).with(event: :run, transition_to: [:running] , from: [:standing])
+    end
+
+    it 'evaluates the transition throught state machine' do
+      expect_any_instance_of(Interstate::StateMachine).to receive(:evaluate_transition_by!)
+      subject.run_standing
     end
   end
 
@@ -73,42 +72,41 @@ RSpec.describe Interstate do
         allow(Run).to receive(:call).with(base_object: subject) { context }
         interstate.on(event: :run, transition_to: [:running] , from: [:standing])
       end
-      context 'when is instantiated' do
-        it 'define a method from event argument' do
-          expect(subject).to respond_to(:run)
+
+      it 'define a method from event argument' do
+        expect(subject).to respond_to(:run)
+      end
+
+      context 'when evaluating the transition' do
+        before do
+          expect(Run).to receive(:call).with(base_object: subject)
         end
-
-        context 'and evaluating the transition' do
-          before do
-            expect(Run).to receive(:call).with(base_object: subject)
-          end
-          context 'when event is successfull' do
-            it 'sends next to state machine' do
-              expect_any_instance_of(Interstate::StateMachine).to receive(:next)
-              subject.run
-            end
-          end
-
-          context 'when event is no successfull' do
-            let(:context) { double('event context', success?: false, error: 'error') }
-
-            it 'does not sends next to state machine' do
-              expect_any_instance_of(Interstate::StateMachine).to_not receive(:next)
-              subject.run
-            end
+        context 'and event is successfull' do
+          it 'sends next to state machine' do
+            expect_any_instance_of(Interstate::StateMachine).to receive(:next)
+            subject.run
           end
         end
 
-        it 'evaluates the transition throught state machine' do
-          expect_any_instance_of(Interstate::StateMachine).to receive(:evaluate_transition_by!)
-          subject.run
-        end
+        context 'and event is no successfull' do
+          let(:context) { double('event context', success?: false, error: 'error') }
 
-        it 'call an event interactor' do
-          allow(Interactor::Context).to receive(:new).with(event: :run, transition_to: [:running] , from: [:standing])
-          subject.run
-          expect(Interactor::Context).to have_received(:new).with(event: :run, transition_to: [:running] , from: [:standing])
+          it 'does not sends next to state machine' do
+            expect_any_instance_of(Interstate::StateMachine).to_not receive(:next)
+            subject.run
+          end
         end
+      end
+
+      it 'evaluates the transition throught state machine' do
+        expect_any_instance_of(Interstate::StateMachine).to receive(:evaluate_transition_by!)
+        subject.run
+      end
+
+      it 'call an event interactor' do
+        allow(Interactor::Context).to receive(:new).with(event: :run, transition_to: [:running] , from: [:standing])
+        subject.run
+        expect(Interactor::Context).to have_received(:new).with(event: :run, transition_to: [:running] , from: [:standing])
       end
     end
     context 'when block is given' do
@@ -127,14 +125,15 @@ RSpec.describe Interstate do
         expect(subject).to respond_to(:cycle)
       end
 
-      context 'when is instantiated' do
+      context 'and an event interactor is defined' do
         before { expect(Cycle).to receive(:call).with(base_object: subject )}
+
         it 'define a method from merging event and from arguments' do
           subject.cycle
           expect(subject).to respond_to(:cycle_off)
         end
 
-        context 'and evaluating the transition' do
+        context 'when evaluating the transition' do
           context 'when event is successfull' do
             it 'sends next to state machine' do
               expect_any_instance_of(Interstate::StateMachine).to receive(:next)
@@ -142,7 +141,7 @@ RSpec.describe Interstate do
             end
           end
 
-          context 'when event is no successfull' do
+          context 'and event is no successfull' do
             let(:context) { double('event context', success?: false, error: 'error') }
 
             it 'does not sends next to state machine' do
@@ -164,7 +163,7 @@ RSpec.describe Interstate do
 
   describe '.transition_table(*states)' do
     it 'sets machine_states variable' do
-      interstate.transition_table :off, :on {}
+      interstate.transition_table(:off, :on) {}
       expect(interstate.machine_states).to eq [:off, :on]
     end
   end
